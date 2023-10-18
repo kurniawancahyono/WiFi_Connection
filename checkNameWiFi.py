81270bb7b4d43f7e8cb540351e3d5693123f4087
Kurniawan Cahyono <kurniawancahyono6@gmail.com>
2023-10-18 22:13:24 +0700

    add check name WiFi

---
+++ checkNameWiFi.py
@@ -0,0 +1,11 @@
+import subprocess
+
+output = subprocess.check_output("netsh wlan show interfaces")
+output = output.decode("utf-8")
+
+start = output.find("SSID") + 6
+end = output.find("\n", start)
+
+wifi_name = output[start:end].strip()
+
+print("Connected to WiFi network:", wifi_name)
